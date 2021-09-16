5a7c049b17227a49579a1a657a197c513dda41cb
yoonminsang <apdlyooapdl@naver.com>
2021-09-16 18:20:55 +0900

    feat: db loader 설정

---
+++ server/src/loaders/db-loader.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import { ConnectionOptions, createConnection } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export default async (): Promise<void> => {
+  const connectionOption: ConnectionOptions = {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    logging: false,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
+    namingStrategy: new SnakeNamingStrategy(),
+  };
+  await createConnection(connectionOption);
+};
