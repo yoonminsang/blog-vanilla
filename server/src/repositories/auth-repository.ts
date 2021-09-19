8f8c183f8dfdad513f6f8ccf04dafbb3f79f5c79
yoonminsang <apdlyooapdl@naver.com>
2021-09-20 01:55:56 +0900

    feat : auth repository 생성(회원가입)

---
+++ server/src/repositories/auth-repository.ts
@@ -0,0 +1,30 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from 'entity/user';
+import { ICheckEmail, ICheckNickname, ICheckUser, ICreateUser } from 'types/auth';
+
+@EntityRepository(User)
+class AuthRepository extends Repository<User> {
+  checkUser({ id }: ICheckUser) {
+    return this.createQueryBuilder('user')
+      .select(['user.id', 'user.email', 'user.nickname'])
+      .where('user.id = :id', { id })
+      .getOne();
+  }
+
+  async checkEmail({ email }: ICheckEmail) {
+    const user = await this.createQueryBuilder('user').where('user.email = :email', { email }).getOne();
+    return !!user;
+  }
+
+  async checkNickname({ nickname }: ICheckNickname) {
+    const user = await this.createQueryBuilder('user').where('user.nickname = :nickname', { nickname }).getOne();
+    return !!user;
+  }
+
+  async createUser({ email, nickname, password }: ICreateUser) {
+    const user = await this.createQueryBuilder().insert().into(User).values({ email, nickname, password }).execute();
+    return user.identifiers[0].id;
+  }
+}
+
+export default AuthRepository;
