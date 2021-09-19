5803133518d456e9d5d71f9bf5e1b44174012959
yoonminsang <apdlyooapdl@naver.com>
2021-09-20 01:52:35 +0900

    feat: 에러 핸들러 설정 추가
    - 컨트롤러별로 파일을 나눌까 생각중

--- server/src/error/error-handler.ts
+++ server/src/error/error-handler.ts
@@ -10,6 +10,15 @@ const errorHandler = (err: CustomError): ErrorType => {
   let errorMessage;
 
   switch (message) {
+    case 'invalid request body':
+      errorMessage = '잘못된 요청입니다';
+      break;
+    case 'duplicate email':
+      errorMessage = '이메일이 존재합니다';
+      break;
+    case 'duplicate nickname':
+      errorMessage = '닉네임이 존재합니다';
+      break;
     default:
       errorMessage = '다시 시도해주세요';
       break;
