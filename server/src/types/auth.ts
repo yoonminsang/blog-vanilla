33abb484d98dca7e6007bfe0445ff21327d68145
yoonminsang <apdlyooapdl@naver.com>
2021-09-20 14:25:28 +0900

    wip: auth 타입 수정

--- server/src/types/auth.ts
+++ server/src/types/auth.ts
@@ -10,9 +10,8 @@ export interface ICheckNickname {
   nickname: string;
 }
 
-// TODO : 이걸 상속하는게 의미있는걸까??
-export interface ICreateUser extends ICheckEmail, ICheckNickname {
-  // email: string;
-  // nickname: string;
+export interface ICreateUser {
+  email: string;
+  nickname: string;
   password: string;
 }
