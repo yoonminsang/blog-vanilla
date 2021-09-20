873f6283936093316829730213f13b02802a8d8e
yoonminsang <apdlyooapdl@naver.com>
2021-09-20 14:24:04 +0900

    feat: 에러처리 함수화

---
+++ server/src/error/error-process.ts
@@ -0,0 +1,10 @@
+import { Response } from 'express';
+import CustomError from './custom-error';
+
+const errorProcess = (res: Response, err: CustomError, errorHandler: any) => {
+  console.log(err);
+  const { status, errorMessage } = errorHandler(err as CustomError);
+  res.status(status).json({ errorMessage });
+};
+
+export default errorProcess;
