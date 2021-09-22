9053b9b17fdf02e7d242ddae3cbc0383983f7149
yoonminsang <apdlyooapdl@naver.com>
2021-09-22 22:38:12 +0900

    wip: jwt미들웨어를 적용해 유저 체크

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -3,23 +3,15 @@ import CustomError from 'error/custom-error';
 import errorProcess from 'error/error-process';
 import { Request, Response } from 'express';
 import AuthService from 'services/auth-service';
-import { getAccessToken, getRefreshToken } from 'utils/jwt';
 
 // TODO : 클래스 인스턴스 바인딩하기!!
 const service = new AuthService();
 
 class AuthController {
-  async autoLogin(req: Request, res: Response) {
+  async checkUser(req: Request, res: Response) {
     try {
-      const accessToken = getAccessToken(req.headers.authorization);
-      const refreshToken = getRefreshToken(req.cookies);
-      const { newAccessToken, newRefreshToken, nickname } = await service.autoLogin({ accessToken, refreshToken });
-
-      if (newRefreshToken) {
-        res.cookie('refreshtoken', refreshToken, { httpOnly: true });
-      }
-
-      res.status(200).json({ nickname, accessToken: newAccessToken });
+      const user = req.user || null;
+      res.status(200).json({ user });
     } catch (err) {
       errorProcess(res, err as CustomError, errorAuth);
     }
@@ -29,7 +21,10 @@ class AuthController {
     try {
       const { email, nickname, password } = req.body;
       const { accessToken, refreshToken } = await service.signup({ email, nickname, password });
-      res.cookie('refreshtoken', refreshToken, { httpOnly: true });
+      res.cookie('refreshtoken', refreshToken, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7일
+      });
       res.status(201).json({ nickname, accessToken });
     } catch (err) {
       errorProcess(res, err as CustomError, errorAuth);
@@ -40,7 +35,10 @@ class AuthController {
     try {
       const { email, password } = req.body;
       const { nickname, accessToken, refreshToken } = await service.login({ email, password });
-      res.cookie('refreshtoken', refreshToken, { httpOnly: true });
+      res.cookie('refreshtoken', refreshToken, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7일
+      });
       res.status(200).json({ nickname, accessToken });
     } catch (err) {
       errorProcess(res, err as CustomError, errorAuth);
