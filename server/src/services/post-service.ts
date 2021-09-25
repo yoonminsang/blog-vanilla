5995823e7972f48600e4c29ac5dc643e2b07785a
yoonminsang <apdlyooapdl@naver.com>
2021-09-26 00:57:48 +0900

    feat: 글쓰기 서비스 계층 구현

---
+++ server/src/services/post-service.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from 'typeorm';
+import errorGenerator from 'error/error-generator';
+import { POST_ERROR_MESSAGE } from 'constants/error-message';
+import PostRepository from 'repositories/post-repository';
+import UserRepository from 'repositories/user-repository';
+
+class PostService {
+  async createPost(title: string, content: string, userId: string) {
+    const existId = await getCustomRepository(UserRepository).checkId(userId);
+    if (!existId) {
+      throw errorGenerator({
+        code: 400,
+        message: POST_ERROR_MESSAGE.notFoundUserId,
+      });
+    }
+    const postId = await getCustomRepository(PostRepository).createPost(title, content, userId);
+    return postId;
+  }
+}
+
+export default PostService;
