54a35ceb7275452a2c4c9f4a4fa0de4b5c4ccbff
yoonminsang <apdlyooapdl@naver.com>
2021-09-26 01:01:32 +0900

    feat: 글쓰기 에러 핸들러 생성

---
+++ server/src/error/error-handler/error-post.ts
@@ -0,0 +1,21 @@
+import { POST_ERROR_MESSAGE } from 'constants/error-message';
+import CustomError from 'error/custom-error';
+import { IError } from 'types/error';
+
+const errorPost = (err: CustomError): IError => {
+  const { status, message } = err;
+  let errorMessage;
+
+  switch (message) {
+    case POST_ERROR_MESSAGE.notFoundUserId:
+      errorMessage = '유저가 존재하지 않습니다';
+      break;
+    default:
+      errorMessage = '다시 시도해주세요';
+      break;
+  }
+
+  return { status, errorMessage };
+};
+
+export default errorPost;
