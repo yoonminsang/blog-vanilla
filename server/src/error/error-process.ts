7c7bd31a908426e9034410435822425218a11b2f
yoonminsang <apdlyooapdl@naver.com>
2021-09-26 20:28:51 +0900

    feat: 에러 미들웨어 생성

--- server/src/error/error-process.ts
+++ server/src/error/error-process.ts
@@ -1,10 +1,34 @@
 import { Response } from 'express';
 import CustomError from './custom-error';
+import errorAuth from './error-handler/error-auth';
+import errorJoi from './error-handler/error-joi';
+import errorJwt from './error-handler/error-jwt';
+import errorPost from './error-handler/error-post';
 
-const errorProcess = (res: Response, err: CustomError, errorHandler: any) => {
+const errorHandler = (err: CustomError) => {
+  switch (err.from) {
+    case 'jwt':
+      return errorJwt(err);
+    case 'joi':
+      return errorJoi(err);
+    case 'auth':
+      return errorAuth(err);
+    case 'post':
+      return errorPost(err);
+    default:
+      return { status: 500, errorMessage: '에러 핸들러가 존재하지 않습니다' };
+  }
+};
+
+const errorProcess = (res: Response, err: CustomError | Error) => {
+  // TODO: console.log는 winstom으로 바꿀지도??
   console.log(err);
-  const { status, errorMessage } = errorHandler(err);
-  res.status(status).json({ errorMessage });
+  if (err instanceof CustomError) {
+    const { status, errorMessage } = errorHandler(err);
+    res.status(status).json({ errorMessage });
+  } else {
+    res.status(500).json({ errorMessage: '서버 에러' });
+  }
 };
 
 export default errorProcess;
