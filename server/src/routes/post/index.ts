cfdbe1c882a60ad9b57d3c53405c5a325eae1d3b
yoonminsang <apdlyooapdl@naver.com>
2021-09-27 19:37:08 +0900

    feat: 글읽기 라우터 구현

--- server/src/routes/post/index.ts
+++ server/src/routes/post/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import PostController from 'controllers/post-controller';
 import postValidation from 'validation/post-validation';
 import isLoggedInMiddleware from 'middlewares/isLoggedInMiddleware';
+import postReadValidation from 'validation/post/post-read-validation';
 
 const postRouter = Router();
 
 const postController = new PostController();
 
 postRouter.post('/', isLoggedInMiddleware, postValidation, postController.createPost);
+postRouter.get('/:id', postReadValidation, postController.readPost);
 
 export default postRouter;
