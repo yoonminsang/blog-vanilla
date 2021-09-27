d2f1a31d42ced36d5c653ad26a330ec2719b3d79
yoonminsang <apdlyooapdl@naver.com>
2021-09-27 18:36:37 +0900

    fefat: 글 읽기 레포지터리 구현

--- server/src/repositories/post-repository.ts
+++ server/src/repositories/post-repository.ts
@@ -4,8 +4,17 @@ import Post from 'entity/post';
 @EntityRepository(Post)
 class PostRepository extends Repository<Post> {
   async createPost(title: string, content: string, userId: string) {
-    const post = await this.createQueryBuilder().insert().into(Post).values({ title, content, user: userId }).execute();
+    const post = await this.createQueryBuilder().insert().into(Post).values({ title, content, userId }).execute();
     return post.identifiers[0].id;
+  }
+
+  async readPost(id: number) {
+    const post = await this.createQueryBuilder('post')
+      .select(['post.id', 'user.nickname'])
+      .innerJoin('post.user', 'user')
+      .where('post.id = :id', { id })
+      .getOne();
+    return post;
   }
 }
 
