8fd74873a1f57f3f670ffbbc3864e9c72d4ce974
yoonminsang <apdlyooapdl@naver.com>
2021-09-29 17:29:18 +0900

    feat: postlist 라우터 구현

--- server/src/routes/post/index.ts
+++ server/src/routes/post/index.ts
@@ -3,6 +3,7 @@ import PostController from 'controllers/post-controller';
 import createPostValidation from 'validation/post/create-post-validation';
 import isLoggedInMiddleware from 'middlewares/isLoggedInMiddleware';
 import readPostValidation from 'validation/post/read-post-validation';
+import readPostListValidation from 'validation/post/read-post-list-validation';
 
 const postRouter = Router();
 
@@ -10,5 +11,6 @@ const postController = new PostController();
 
 postRouter.post('/', isLoggedInMiddleware, createPostValidation, postController.createPost);
 postRouter.get('/:id', readPostValidation, postController.readPost);
+postRouter.get('/', readPostListValidation, postController.readPostList);
 
 export default postRouter;
