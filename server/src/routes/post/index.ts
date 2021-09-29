6b468598f6505a147079d2a3b1092adb4caf1fc8
yoonminsang <apdlyooapdl@naver.com>
2021-09-29 21:00:09 +0900

    feat: 글 업데이트 라우터

--- server/src/routes/post/index.ts
+++ server/src/routes/post/index.ts
@@ -2,15 +2,16 @@ import { Router } from 'express';
 import PostController from 'controllers/post-controller';
 import createPostValidation from 'validation/post/create-post-validation';
 import isLoggedInMiddleware from 'middlewares/isLoggedInMiddleware';
-import readPostValidation from 'validation/post/read-post-validation';
 import readPostListValidation from 'validation/post/read-post-list-validation';
+import idParamsValidation from 'validation/common/id-params-validation';
 
 const postRouter = Router();
 
 const postController = new PostController();
 
 postRouter.post('/', isLoggedInMiddleware, createPostValidation, postController.createPost);
-postRouter.get('/:id', readPostValidation, postController.readPost);
+postRouter.get('/:id', idParamsValidation, postController.readPost);
 postRouter.get('/', readPostListValidation, postController.readPostList);
+postRouter.put('/:id', isLoggedInMiddleware, idParamsValidation, postController.updatePost);
 
 export default postRouter;
