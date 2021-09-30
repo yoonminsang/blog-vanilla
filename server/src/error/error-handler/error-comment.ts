0902bc43c4f3431b7a21d83e6c36fe16c435f7a4
yoonminsang <apdlyooapdl@naver.com>
2021-09-30 20:33:40 +0900

    feat: 댓글 수정 에러 핸들링

--- server/src/error/error-handler/error-comment.ts
+++ server/src/error/error-handler/error-comment.ts
@@ -15,6 +15,9 @@ const errorComment = (err: CustomError): IError => {
     case COMMENT_ERROR_MESSAGE.notFoundCommentList:
       errorMessage = '댓글 목록이 존재하지 않습니다';
       break;
+    case COMMENT_ERROR_MESSAGE.diffrentUserId:
+      errorMessage = '글 작성자와 아이디가 다릅니다';
+      break;
     default:
       errorMessage = '다시 시도해주세요';
       break;
