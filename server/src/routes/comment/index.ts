b4fc5a405286516ec70c80799215caaa11c13e44
yoonminsang <apdlyooapdl@naver.com>
2021-09-30 18:11:16 +0900

    feat: 댓글 읽기 라우터

--- server/src/routes/comment/index.ts
+++ server/src/routes/comment/index.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import isLoggedInMiddleware from 'middlewares/isLoggedInMiddleware';
 import CommentController from 'controllers/comment-controller';
 import createCommentValidation from 'validation/comment/create-comment-validation';
+import idParamsValidation from 'validation/common/id-params-validation';
+import readCommentListValidation from 'validation/comment/read-comment-list-validation';
 
 const commentRouter = Router();
 
 const commentController = new CommentController();
 
 commentRouter.post('/', isLoggedInMiddleware, createCommentValidation, commentController.createComment);
+commentRouter.get('/:id', idParamsValidation, commentController.readComment);
+commentRouter.get('/', readCommentListValidation, commentController.readCommentList);
 
 export default commentRouter;
