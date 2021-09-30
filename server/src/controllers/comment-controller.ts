320629d79f63a221b388137f2a7898fdb0f4fb64
yoonminsang <apdlyooapdl@naver.com>
2021-09-30 18:09:19 +0900

    feat: 댓글 읽기 컨트롤러

--- server/src/controllers/comment-controller.ts
+++ server/src/controllers/comment-controller.ts
@@ -18,6 +18,27 @@ class CommentController {
       next(err);
     }
   }
+
+  async readComment(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    try {
+      const comment = await service.readComent(+id);
+      res.status(200).json({ comment });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async readCommentList(req: Request, res: Response, next: NextFunction) {
+    const postId = req.query.postId as string;
+    const pageId = req.query.pageId as string;
+    try {
+      const commentList = await service.readCommentList(+postId, +pageId);
+      res.status(200).json({ commentList });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 export default CommentController;
