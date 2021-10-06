dea1f31e09239299251175794974341185b58614
yoonminsang <apdlyooapdl@naver.com>
2021-10-07 00:45:22 +0900

    feat: app에 라우터 적용

--- client/src/app.js
+++ client/src/app.js
@@ -1,55 +1,26 @@
-import Component from './lib/component.js';
-import userStore from './store/user-store.js';
+import Router from './lib/router';
+import LoginPage from './pages/login-page';
+import MainPage from './pages/main-page';
+import NotFoundPage from './pages/not-found-page';
+import PostPage from './pages/post-page';
+import SignupPage from './pages/signup-page';
 
-const IncreaseBtn = () => `<button class="increase">증가</button>`;
-const DecreaseBtn = () => `<button class="decrease">감소</button>`;
-const Output = count => `<h1 class="output">${count}</h1>`;
-const Text = text => `<input type="text" class="text" value="${text}"/>`;
-const LoginBtn = () => `<button class="login">로그인</button>`;
-const LogoutBtn = () => `<button class="logout">로그아웃</button>`;
-const UserText = user => `<div>${user}</div>`;
-
-class App extends Component {
-  setup() {
-    this.state = {
-      count: 0,
-      text: '',
-      user: undefined,
-    };
+class App {
+  constructor(target) {
+    this.target = target;
+    this.routes = [
+      { path: '/', component: MainPage },
+      { path: '/login', component: LoginPage },
+      { path: '/signup', component: SignupPage },
+      { path: '/post/:id', component: PostPage },
+    ];
+    this.NotFoundPage = NotFoundPage;
+    this.render();
   }
 
-  markup() {
-    return `
-      ${IncreaseBtn()}
-      ${DecreaseBtn()}
-      ${Output(this.state.count)}
-      ${Text(this.state.text)}
-      ${LoginBtn()}
-      ${LogoutBtn()}
-      ${UserText(this.state.user)}
-    `;
-  }
-
-  componentDidMount() {
-    userStore.subscribe(() => this.setState({ user: userStore.state.user }));
-  }
-
-  setEvent() {
-    this.addEvent('click', '.increase', () => {
-      this.setState({ count: this.state.count + 1 });
-    });
-    this.addEvent('click', '.decrease', () => {
-      this.setState({ count: this.state.count - 1 });
-    });
-    this.addEvent('input', '.text', ({ target }) => {
-      this.setState({ text: target.value });
-    });
-    this.addEvent('click', '.login', () => {
-      userStore.login('minsang');
-    });
-    this.addEvent('click', '.logout', () => {
-      userStore.logout();
-    });
+  render() {
+    new Router(this.target, this.routes, this.NotFoundPage);
   }
 }
+
 export default App;
