61673137985d0fc9a32b61cb33777715f1016261
yoonminsang <apdlyooapdl@naver.com>
2021-10-07 00:43:13 +0900

    wip: 옵저버블 클래스 변수명 수정

--- client/src/lib/basic-observable.js
+++ client/src/lib/basic-observable.js
@@ -1,23 +1,28 @@
 class Observable {
   constructor() {
-    this._observers = new Set();
+    this.observers = new Set();
   }
+
   setState(nextState) {
     this.state = { ...this.state, ...nextState };
     this.notify();
   }
+
   subscribe(observer) {
     observer();
-    this._observers.add(observer);
+    this.observers.add(observer);
   }
+
   unsubscribe(observer) {
-    this._observers = [...this._observers].filter(subscriber => subscriber !== observer);
+    this.observers = [...this.observers].filter(subscriber => subscriber !== observer);
   }
+
   unsubscribeAll() {
-    this._observers = new Set();
+    this.observers = new Set();
   }
+
   notify() {
-    this._observers.forEach(observer => observer());
+    this.observers.forEach(observer => observer());
   }
 }
 
