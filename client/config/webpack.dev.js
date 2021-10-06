2f904b314fdabd005815d279d97cf8b28df7faa2
yoonminsang <apdlyooapdl@naver.com>
2021-10-07 00:43:45 +0900

    feat: 웹팩 설정

---
+++ client/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '/dist'),
+    index: 'index.html',
+    port: 9000,
+    writeToDisk: true,
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
