c247ad9bd20fc8fa695ebe75a697d154fbcebec8
yoonminsang <apdlyooapdl@naver.com>
2021-10-08 01:23:30 +0900

    wip: 메인페이지에 버튼적용(수정예정)

--- client/src/pages/main-page.js
+++ client/src/pages/main-page.js
@@ -1,3 +1,4 @@
+import Button from '../components/common/button';
 import Main from '../components/main';
 import Main2 from '../components/main2';
 import Component from '../lib/component';
@@ -7,15 +8,18 @@ class MainPage extends Component {
     return /* html */ `
       <div class="main"></div>
       <div class="main2"></div>
+      <div class="button-div"></div>
     `;
   }
 
   appendComponent() {
     const $main = document.querySelector('.main');
     const $main2 = document.querySelector('.main2');
+    const $buttonDiv = document.querySelector('.button-div');
 
     new Main($main);
     new Main2($main2);
+    new Button($buttonDiv, { text: '버트은', class: 'full-width blue' });
   }
 }
 
