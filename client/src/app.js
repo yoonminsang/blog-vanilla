de6cfccd26c6be56cc35e33227635f996e698ff0
yoonminsang <apdlyooapdl@naver.com>
2021-10-11 18:25:34 +0900

    wip: 엔트리 파일 수정

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,7 @@ import { Router } from 'ms-vanilla';
 import LoginPage from './pages/login-page';
 import MainPage from './pages/main-page';
 import NotFoundPage from './pages/not-found-page';
+import PostListPage from './pages/post-list-page';
 import PostPage from './pages/post-page';
 import SignupPage from './pages/signup-page';
 
@@ -9,7 +10,8 @@ class App {
   constructor(target) {
     this.target = target;
     this.routes = [
-      { path: '/', component: MainPage },
+      { path: '/main', component: MainPage },
+      { path: '/', component: PostListPage },
       { path: '/login', component: LoginPage },
       { path: '/signup', component: SignupPage },
       { path: '/post/:id', component: PostPage },
