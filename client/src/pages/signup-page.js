55ad1e25afbad7a1df5230914e394e6a24f1a1f6
yoonminsang <apdlyooapdl@naver.com>
2021-10-14 01:38:06 +0900

    feat: 회원가입 페이지

--- client/src/pages/signup-page.js
+++ client/src/pages/signup-page.js
@@ -1,11 +1,25 @@
 // import { Component } from 'ms-vanilla';
 import Component from '../components/lib/component';
+import Auth from '../components/auth';
+import Header from '../components/common/header';
 
 class SignupPage extends Component {
+  setup() {
+    this.state = { user: undefined };
+  }
+
   markup() {
     return /* html */ `
-      <div>회원가입 페이지</div>
+      <inside class="header-inside"></inside>
+      <inside class="login-inside"></inside>
     `;
+  }
+
+  appendComponent(target) {
+    const $header = target.querySelector('.header-inside');
+    const $login = target.querySelector('.login-inside');
+    new Header($header);
+    new Auth($login, { login: false });
   }
 }
 
