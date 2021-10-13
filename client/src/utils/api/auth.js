d65e70756e90ad3eb8733dae4322e4e405dbf66e
yoonminsang <apdlyooapdl@naver.com>
2021-10-13 23:25:29 +0900

    refactor: auth api 함수명 변경

--- client/src/utils/api/auth.js
+++ client/src/utils/api/auth.js
@@ -1,11 +1,11 @@
 import request from './request';
 
-export const login = ({ id, password }) => request('POST', '/api/auth', { id, password });
+export const loginApi = ({ email, password }) => request('POST', '/api/auth/login', { email, password });
 
-export const signup = ({ id, password }) => request('POST', '/api/users', { id, password });
+export const signupApi = ({ email, password }) => request('POST', '/api/auth/signup', { email, password });
 
-export const logout = () => request('DELETE', '/api/auth');
+export const logoutApi = () => request('DELETE', '/api/auth');
 
-export const checkAuth = () => request('GET', '/api/auth');
+export const checkAuthApi = () => request('GET', '/api/auth');
 
 // export const githubLogin = ({ code }) => request('POST', '/api/auth/github', { code });
