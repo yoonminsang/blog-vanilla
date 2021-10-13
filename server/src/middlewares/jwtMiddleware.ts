eae19800b0b7d2df0996d6de23366e934f6be7d6
yoonminsang <apdlyooapdl@naver.com>
2021-10-13 23:26:26 +0900

    feat: jwt 미들웨어 변경
    - 토큰 만료에 따른 분기 처리, decode 오류 핸들링

--- server/src/middlewares/jwtMiddleware.ts
+++ server/src/middlewares/jwtMiddleware.ts
@@ -1,46 +1,42 @@
-import { JWT_ERROR_MESSAGE } from 'constants/error-message';
-import errorGenerator from 'error/error-generator';
-import errorProcess from 'error/error-process';
 import { NextFunction, Request, Response } from 'express';
 import { createToken, decodeToken, getAccessToken, getRefreshToken } from 'utils/jwt';
-
-const FROM = 'jwt';
 
 const jwtMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const accessToken = getAccessToken(req.headers.authorization);
   const refreshToken = getRefreshToken(req.cookies);
+
   if (!accessToken || !refreshToken) {
     return next();
   }
 
-  const { exp: aExp, id: aId, nickname: aNickname } = decodeToken('access', accessToken as string);
-  const { exp: rExp, id: rId, nickname: rNickname } = decodeToken('refresh', refreshToken as string);
+  const { id: aId, nickname: aNickname, jwtError: aError } = await decodeToken('access', accessToken as string);
+  const {
+    exp: rExp,
+    id: rId,
+    nickname: rNickname,
+    jwtError: rError,
+  } = await decodeToken('refresh', refreshToken as string);
+
+  if (aError || rError) {
+    if (aError === 'TokenExpiredError' && !rError) {
+      const newAccessToken = createToken('access', { id: rId, nickname: rNickname });
+      return res.status(200).json({ requestAgain: true, newAccessToken });
+    }
+    if (rError === 'TokenExpiredError') {
+      return res.status(200).json({ expiredRefreshToken: true });
+    }
+    return next();
+  }
+
   if (aId !== rId || aNickname !== rNickname) {
-    const err = errorGenerator({
-      status: 401,
-      message: JWT_ERROR_MESSAGE.invalidToken,
-      from: FROM,
-    });
-    errorProcess(res, err);
+    return next();
   }
 
   const [id, nickname] = [aId, aNickname];
   req.user = { id, nickname };
 
   const now = Math.floor(Date.now() / 1000);
-  if ((aExp as number) - now < 0) {
-    const newAccessToken = createToken('access', { id, nickname });
-    res.status(200).json({ requestAgain: true, newAccessToken }).end();
-  }
-  if ((rExp as number) - now < 0) {
-    // TODO: expiredToken:true를 줄까?? 쿠키, 로컬 값을 제거해야될까??
-    const err = errorGenerator({
-      status: 401,
-      message: JWT_ERROR_MESSAGE.expiredToken,
-      from: FROM,
-    });
-    errorProcess(res, err);
-  }
+
   if ((rExp as number) - now < 60 * 60 * 24 * 3.5) {
     // 3.5일
     const newRefreshToken = createToken('refresh', { id, nickname });
