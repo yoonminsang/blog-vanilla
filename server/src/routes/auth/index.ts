2b07bead833a234851877f1ce5d6edcaec09ff91
yoonminsang <apdlyooapdl@naver.com>
2021-10-15 01:44:30 +0900

    feat: 서버 로그인 검증 추가

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AuthController from 'controllers/auth-controller';
 import signupValidation from 'validation/auth/signup-validation';
+import loginValidation from 'validation/auth/login-validation';
 
 const authRouter = Router();
 
@@ -8,7 +9,7 @@ const authController = new AuthController();
 
 authRouter.get('/', authController.checkUser);
 authRouter.post('/signup', signupValidation, authController.signup);
-authRouter.post('/login', authController.login);
+authRouter.post('/login', loginValidation, authController.login);
 authRouter.delete('/', authController.logout);
 
 export default authRouter;
