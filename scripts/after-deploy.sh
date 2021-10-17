1ba1795dcb8f91b535654604396c56efb288a172
yoonminsang <apdlyooapdl@naver.com>
2021-10-18 01:42:52 +0900

    wip: 배포테스트

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/blog-vanilla
 cd $REPOSITORY
-
-cd client
-sudo cp -r dist/* /var/www/html/
-sudo service nginx restart
-
-cd $REPOSITORY
-cd server
-yarn install --frozen-lockfile
-yarn build
-pm2 reload api
