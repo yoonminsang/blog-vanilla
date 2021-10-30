355ef4c8ff03428d240423b59089c55c2766b357
yoonminsang <apdlyooapdl@naver.com>
2021-10-30 18:01:36 +0900

    fix: after-deploy 경로 오류 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,10 +2,9 @@
 REPOSITORY=/home/ubuntu/blog-vanilla
 
 cd $REPOSITORY
-sudo cp -r dist/* /var/www/html/
+sudo cp -r client/dist/* /var/www/html/
 sudo service nginx restart
 
-cd $REPOSITORY
 cd server
 yarn install --frozen-lockfile
 pm2 kill
