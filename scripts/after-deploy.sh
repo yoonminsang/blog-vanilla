02d6a8b9eb18b05927af6613ef5be4a8e6429f6b
yoonminsang <apdlyooapdl@naver.com>
2021-10-30 22:51:45 +0900

    wip: after-deploy 설정 변경

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/blog-vanilla
 
-cd $REPOSITORY
-sudo cp -r client/dist/* /var/www/html/
-sudo service nginx restart
-
 cd server
 yarn install --frozen-lockfile
-pm2 kill
-yarn prod
+pm2 reload api
