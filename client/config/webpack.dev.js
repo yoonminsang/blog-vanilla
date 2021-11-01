6ea14b62697d1074847594cee38882f71f6862b9
yoonminsang <apdlyooapdl@naver.com>
2021-11-01 21:58:19 +0900

    feat: webpack devserver 옵션 변경

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -17,12 +17,13 @@ module.exports = merge(common, {
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
     index: 'index.html',
+    overlay: true,
     port: 9000,
     writeToDisk: true,
     hot: true,
     historyApiFallback: true,
     proxy: {
-      '/api/': {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
