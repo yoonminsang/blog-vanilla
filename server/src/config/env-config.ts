de3eff18b9b7ade197c7cb8c81d25ffc442bfe3d
yoonminsang <apdlyooapdl@naver.com>
2021-11-04 03:57:05 +0900

    feat: dot env config 이름 및 설정 변경

--- server/src/config/env-config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const envConfig = () =>
-  dotenv.config({
-    path: path.resolve(process.cwd(), process.env.NODE_ENV === 'development' ? '.dev.env' : '.env'),
-  });
-export default envConfig;
