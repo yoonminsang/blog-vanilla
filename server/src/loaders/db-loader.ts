6d8e66e2556ba1bdaba9c886bd3e1fbcbadab7b3
yoonminsang <apdlyooapdl@naver.com>
2021-11-04 04:00:06 +0900

    reafacotr: db 로더의 옵션 분리

--- server/src/loaders/db-loader.ts
+++ server/src/loaders/db-loader.ts
@@ -1,29 +1,10 @@
+import dbConfig from '@/config/db-config';
 import 'reflect-metadata';
-import { ConnectionOptions, createConnection } from 'typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { createConnection } from 'typeorm';
 
 const dbLoader = async (): Promise<void> => {
   try {
-    const connectionOption: ConnectionOptions = {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      synchronize: true,
-      logging: false,
-      entities: ['src/entity/**/*.ts'],
-      migrations: ['src/migration/**/*.ts'],
-      subscribers: ['src/subscriber/**/*.ts'],
-      cli: {
-        entitiesDir: 'src/entity',
-        migrationsDir: 'src/migration',
-        subscribersDir: 'src/subscriber',
-      },
-      namingStrategy: new SnakeNamingStrategy(),
-    };
-    await createConnection(connectionOption);
+    await createConnection(dbConfig());
   } catch (err) {
     console.log('db connection error \n', err);
   }
