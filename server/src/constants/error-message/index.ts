112f4fda4cbf4d04187f6dfb8b8590a070764e1a
yoonminsang <apdlyooapdl@naver.com>
2021-11-07 20:01:37 +0900

    refactor: 에러 메세지부분 리팩토링
    - 원래는 개발자를 위한 에러만 나타냈지만 통합테스트에서 유저를 위한 에러 메세지를 import 하는 경우가 생겨서 수정

--- server/src/constants/error-message/index.ts
+++ server/src/constants/error-message/index.ts
@@ -1,3 +1,5 @@
+// 각각의 객체안의 value는 [개발자를 위한 에러 메세지, 유저를 위한 에러 메세지] 배열로 구성
+
 const JWT_ERROR_MESSAGE = {
   invalidToken: 'invalid token',
   expiredToken: 'expired token',
@@ -10,11 +12,11 @@ const JOI_ERROR_MESSAGE = {
 };
 
 const AUTH_ERROR_MESSAGE = {
-  duplicateEmail: 'duplicate email',
-  duplicateNickname: 'duplicate nickname',
-  notFoundUser: 'not found user',
-  notFoundEmail: 'not found email',
-  notFoundPassword: 'not found password',
+  duplicateEmail: ['duplicate email', '이메일이 존재합니다'],
+  duplicateNickname: ['duplicate nickname', '닉네임이 존재합니다'],
+  notFoundUser: ['not found user', '유저가 존재하지 않습니다'],
+  notFoundEmail: ['not found email', '이메일 또는 비밀번호가 일치하지 않습니다'],
+  notCorrectPassword: ['not found password', '이메일 또는 비밀번호가 일치하지 않습니다'],
 };
 
 const POST_ERROR_MESSAGE = {
