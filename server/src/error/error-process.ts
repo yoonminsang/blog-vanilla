9d771052a964b999716506cc5bb6b9445b492bcb
yoonminsang <apdlyooapdl@naver.com>
2021-11-07 20:31:11 +0900

    wip: 오타수정, jwt 에러핸들러 삭제, 주석추가

--- server/src/error/error-process.ts
+++ server/src/error/error-process.ts
@@ -3,13 +3,11 @@ import CustomError from './custom-error';
 import errorAuth from './error-handler/error-auth';
 import errorComment from './error-handler/error-comment';
 import errorJoi from './error-handler/error-joi';
-import errorJwt from './error-handler/error-jwt';
 import errorPost from './error-handler/error-post';
 
 const errorHandler = (err: CustomError) => {
+  // TODO: common을 만들까?? 고민해보자
   switch (err.from) {
-    case 'jwt':
-      return errorJwt(err);
     case 'joi':
       return errorJoi(err);
     case 'auth':
@@ -19,17 +17,20 @@ const errorHandler = (err: CustomError) => {
     case 'comment':
       return errorComment(err);
     default:
+      // TODO: winston
       return { status: 500, errorMessage: '에러 핸들러가 존재하지 않습니다' };
   }
 };
 
 const errorProcess = (res: Response, err: CustomError | Error) => {
-  // TODO: console.log는 winstom으로 바꿀지도??
-  console.log(err);
+  if (process.env.NODE_ENV === 'development') {
+    console.error(err);
+  }
   if (err instanceof CustomError) {
     const { status, errorMessage } = errorHandler(err);
     res.status(status).json({ errorMessage });
   } else {
+    // TODO: winston으로 err + 시간 기록
     res.status(500).json({ errorMessage: '서버 에러' });
   }
 };
