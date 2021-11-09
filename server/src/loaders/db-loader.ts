45591fc1423ff85c5efaeac61c6486abd07e7545
yoonminsang <apdlyooapdl@naver.com>
2021-11-10 00:18:29 +0900

    feat: db loader에서 유저가 존재하지 않을 시 seed 생성

--- server/src/loaders/db-loader.ts
+++ server/src/loaders/db-loader.ts
@@ -1,12 +1,26 @@
 import dbConfig from '@/config/db-config';
+import logger from '@/config/logger';
+import User from '@/entity/user';
+import commentSeed from '@/seeds/comment.seed';
+import postSeed from '@/seeds/post.seed';
+import userSeed from '@/seeds/user.seed';
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import { createConnection, getRepository } from 'typeorm';
 
 const dbLoader = async (): Promise<void> => {
   try {
     await createConnection(dbConfig());
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne();
+    if (process.env.NODE_ENV !== 'test' && !user) {
+      console.time('data seed');
+      await userSeed();
+      await postSeed();
+      await commentSeed();
+      console.timeEnd('data seed');
+    }
   } catch (err) {
-    console.log('db connection error \n', err);
+    logger.error('db connection error \n', err);
   }
 };
 
