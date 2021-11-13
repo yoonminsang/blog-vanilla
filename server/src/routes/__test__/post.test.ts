1cc8bec4fec739ed1816722999f258d7f351b196
yoonminsang <apdlyooapdl@naver.com>
2021-11-14 02:03:15 +0900

    feat: 기본 post 통합테스트

--- server/src/routes/__test__/post.test.ts
+++ server/src/routes/__test__/post.test.ts
@@ -1,5 +1,5 @@
 import App from '@/app';
-import ERROR_JOI_MESSAGE from '@/constants/error-joi-message.ts';
+// import ERROR_JOI_MESSAGE from '@/constants/error-joi-message.ts';
 import supertest from 'supertest';
 import testConnection from './test-connection';
 
@@ -39,5 +39,91 @@ describe('auth', () => {
       expect(res.status).toBe(200);
       expect(res.body.postId).not.toBeNaN();
     });
+  });
+
+  test('readPost', async () => {
+    const signupData = {
+      email: 'email@naver.com',
+      nickname: 'nickname',
+      password: 'qwer1234!Q',
+    };
+    const signupRes = await agent.post('/api/auth/signup').send(signupData);
+    const { accessToken } = signupRes.body;
+
+    const createPostData = {
+      title: 'title',
+      content: 'content',
+    };
+    const createRes = await agent.post('/api/post').set('Authorization', `Bearer ${accessToken}`).send(createPostData);
+    const { postId } = createRes.body;
+
+    const res = await request.get(`/api/post/${postId}`);
+
+    expect(res.body.post).toMatchObject(createPostData);
+    expect(res.status).toBe(200);
+  });
+
+  test('readPostList', async () => {
+    const signupData = {
+      email: 'email@naver.com',
+      nickname: 'nickname',
+      password: 'qwer1234!Q',
+    };
+    const signupRes = await agent.post('/api/auth/signup').send(signupData);
+    const { accessToken } = signupRes.body;
+
+    const createPostData = {
+      title: 'title',
+      content: 'content',
+    };
+    await agent.post('/api/post').set('Authorization', `Bearer ${accessToken}`).send(createPostData);
+
+    const res = await request.get(`/api/post`);
+    expect(res.body.postList[0]).toMatchObject(createPostData);
+    expect(res.status).toBe(200);
+  });
+
+  test('updatePostList', async () => {
+    const signupData = {
+      email: 'email@naver.com',
+      nickname: 'nickname',
+      password: 'qwer1234!Q',
+    };
+    const signupRes = await agent.post('/api/auth/signup').send(signupData);
+    const { accessToken } = signupRes.body;
+
+    const createPostData = {
+      title: 'title',
+      content: 'content',
+    };
+    const createRes = await agent.post('/api/post').set('Authorization', `Bearer ${accessToken}`).send(createPostData);
+    const { postId } = createRes.body;
+
+    const res = await agent
+      .put(`/api/post/${postId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(createPostData);
+    expect(res.body.postId).not.toBeNaN();
+    expect(res.status).toBe(200);
+  });
+
+  test('deletePostList', async () => {
+    const signupData = {
+      email: 'email@naver.com',
+      nickname: 'nickname',
+      password: 'qwer1234!Q',
+    };
+    const signupRes = await agent.post('/api/auth/signup').send(signupData);
+    const { accessToken } = signupRes.body;
+
+    const createPostData = {
+      title: 'title',
+      content: 'content',
+    };
+    const createRes = await agent.post('/api/post').set('Authorization', `Bearer ${accessToken}`).send(createPostData);
+    const { postId } = createRes.body;
+
+    const res = await agent.delete(`/api/post/${postId}`).set('Authorization', `Bearer ${accessToken}`);
+    expect(res.status).toBe(200);
   });
 });
